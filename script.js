2c244deeda6b3721e55d810edbc45803bc089ca3
madugushivakumar <shivakumarmudiraj565@gmail.com>
2026-04-07 23:16:32 +0530

    Task Manager logic

--- script.js
+++ script.js
@@ -237,23 +237,7 @@ document.querySelectorAll(".priority button").forEach(btn => {
 loadFromLocal();
 showTasks();
 document.querySelectorAll(".filter").forEach(btn => {
-  // ===== VIEW TOGGLE (LIST / CALENDAR) =====
-document.querySelectorAll(".view-btn").forEach(btn => {
   btn.addEventListener("click", () => {
-     document.querySelector(".view-btn.active")?.classList.remove("active");
-    btn.classList.add("active");
-
-    currentView = btn.dataset.view;
-
-    if (currentView === "list") {
-      document.getElementById("taskList").style.display = "block";
-      document.getElementById("calendarView").classList.add("hidden");
-    } else {
-      document.getElementById("taskList").style.display = "none";
-      document.getElementById("calendarView").classList.remove("hidden");
-      renderCalendar();
-    }
-  });
 
     document.querySelector(".filter.active")?.classList.remove("active");
     btn.classList.add("active");
